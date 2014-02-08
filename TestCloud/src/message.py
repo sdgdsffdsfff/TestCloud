eb23b8f31b593d7c0583971d49f6b0d16f9a6dc1
tangmin <doctang@163.com>
2014-02-08 22:03:44 +0800

    Refactor something

--- TestCloud/src/message.py
+++ TestCloud/src/message.py
@@ -5,10 +5,57 @@ Created on 2014-2-4
 @author: Administrator
 '''
 
+import datetime
 import json
 import os
 import re
 import zipfile
+
+DEF_TESTS = [
+    '{"id": 1, "type": 7001, "params": "2000"}',
+    '{"id": 2, "type": 7002, "params": "100 10"}',
+    '{"id": 3, "type": 7003, "params": "100 10"}',
+    '{"id": 4, "type": 7004, "params": "100 10"}',
+    '{"id": 5, "type": 7005, "params": "100 10"}',
+    '{"id": 6, "type": 7006, "params": "100 10"}',
+    '{"id": 7, "type": 7007, "params": "100 10"}',
+    '{"id": 8, "type": 7008, "params": "--ei times 1"}'
+]
+
+OPT_TESTS = {
+    7001: {
+        'name': 'monkey',
+        'cmd': 'monkey -s 25 --ignore-timeouts --ignore-crashes -v {0} --pct-nav 0 --pct-trackball 0'
+    },
+    7002: {
+        'name': 'monkey-com.android.gallery3d',
+        'cmd': 'sh start_monkey_exit.sh com.android.gallery3d com.android.camera.CameraLauncher {0}'
+    },
+    7003: {
+        'name': 'monkey-com.android.contacts.people',
+        'cmd': 'sh start_monkey_exit.sh com.android.contacts com.android.contacts.activities.PeopleActivity {0}'
+    },
+    7004: {
+        'name': 'monkey-com.android.mms',
+        'cmd': 'sh start_monkey_exit.sh com.android.mms com.android.mms.ui.FunctionSwitchActivity {0}'
+    },
+    7005: {
+        'name': 'monkey-com.android.contacts.dialer',
+        'cmd': 'sh start_monkey_exit.sh com.android.contacts com.android.contacts.DialerActivity {0}'
+    },
+    7006: {
+        'name': 'monkey-com.android.settings',
+        'cmd': 'sh start_monkey_exit.sh com.android.settings com.android.settings.Settings {0}'
+    },
+    7007: {
+        'name': 'monkey-com.android.strengthenmusic',
+        'cmd': 'sh start_monkey_exit.sh com.android.strengthenmusic com.android.strengthenmusic.ui.MusicWelcome {0}'
+    },
+    7008: {
+        'name': 'stress',
+        'cmd': 'am start -n com.ztemt.test.auto/.AutoTestActivity --es mode auto {0}'
+    }
+}
 
 class Message:
 
@@ -35,8 +82,8 @@ def getBuildProp(z, prop):
 def getBuildDate(z):
     date = getBuildProp(z, 'ro.build.date')
     r = re.compile('\d+')
-    l = r.findall(date)
-    return '{0}-{1}-{2} {3}:{4}:{5}'.format(*l)
+    p = r.findall(date)
+    return '{0}-{1}-{2} {3}:{4}:{5}'.format(*p)
 
 def getUpdateInfo(model, buildDate):
     parent = '\\\\10.204.75.220\\htdocs$\\{0}'.format(model)
@@ -47,6 +94,34 @@ def getUpdateInfo(model, buildDate):
         date = getBuildDate(z)
         if date > buildDate:
             return ('http://10.204.75.220:8181/{0}/{1}'.format(model, name), date)
+
+def sendTask(device, update=True, tests=None):
+    queue = []
+    msg = Message(6003)
+    updateInfo = getUpdateInfo(device.model, device.buildDate)
+    if updateInfo and update:
+        queue.append({'type': 7001, 'updateUrl': updateInfo[0], 'buildDate': updateInfo[1]})
+    buildDate = ''.join([s for s in device.buildDate if s.isdigit()])
+    timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    items = []
+    tests = tests if tests else DEF_TESTS
+    for test in tests:
+        d = dict(json.loads(test))
+        i = d['id']
+        t = d['type']
+        params = d['params']
+        name = OPT_TESTS[t]['name']
+        cmd = OPT_TESTS[t]['cmd'].format(params)
+        paths = (device.deviceId, device.build, buildDate, timestamp, name)
+        items.append({
+            'id': i,
+            'name': name,
+            'cmd': cmd,
+            'uploadPath': '/pub/log/{0}/{1}/{2}/{3}-{4}.txt'.format(*paths)
+        })
+    queue.append({'type': 7002, 'tests': str(items)})
+    msg.put('queue', queue)
+    device.protocol.sendMessage(str(msg).encode('utf-8'))
 
 if __name__ == '__main__':
     print getUpdateInfo('NX503A', '2014-01-04 12:12:12')
--- TestCloud/src/server.py
+++ TestCloud/src/server.py
@@ -6,12 +6,9 @@ Created on 2014-1-9
 
 from autobahn.twisted.websocket import WebSocketServerProtocol, \
                                        WebSocketServerFactory
-import sys
 import json
-import datetime
-import os
-import zipfile
-import re
+import message
+import sys
 
 class Device:
 
@@ -53,27 +50,11 @@ class Client:
                 'deviceId': self.deviceId
         }
 
-class Message:
-
-    def __init__(self, msg=None):
-        self.json = dict()
-        if type(msg) == int:
-            self.json['type'] = msg
-
-    def put(self, key, value):
-        self.json[key] = value
-
-    def get(self, key):
-        self.json.get(key)
-
-    def __str__(self):
-        return json.dumps(self.json)
-
 clients = dict()
 
 def broadcastDevices():
     online = []
-    msg = Message(6001)
+    msg = message.Message(6001)
     for key in devices:
         online.append(devices[key].getDevice())
     msg.put('devices', online)
@@ -97,7 +78,7 @@ class TestClientProtocol(WebSocketServerProtocol):
         if d['type'] == 6001:
             broadcastDevices()
         elif d['type'] == 6002:
-            sendTask(devices[d['deviceId']], bool(d['update']), d['tests'])
+            message.sendTask(devices[d['deviceId']], bool(d['update']), d['tests'])
 
     def onClose(self, wasClean, code, reason):
         print('WebSocket connection closed: {0}'.format(reason))
@@ -105,103 +86,6 @@ class TestClientProtocol(WebSocketServerProtocol):
             clients.pop(self.deviceId)
 
 devices = dict()
-
-def getUpdateUrl(device):
-    path = '\\\\10.204.75.220\\htdocs$\\{0}'
-    if not os.path.exists(path.format(device.model)):
-        return None
-    update_zip = max(os.listdir(path.format(device.model)))
-    if update_zip:
-        update_path = os.path.join(path.format(device.model), update_zip)
-        z = zipfile.ZipFile(update_path, 'r')
-        b = z.read('system/build.prop')
-        s = b.decode('utf-8')
-        l = s.split('\n')
-        for ll in l:
-            if ll.startswith('ro.build.date='):
-                v = ll[14:]
-                p = re.compile('\d+')
-                vv = p.findall(v)
-                date = '{0} {1}'.format('-'.join(vv[:3]), ':'.join(vv[3:]))
-                if date > device.buildDate:
-                    return ('http://10.204.75.220:8181/{0}/{1}'.format(device.model, update_zip), date)
-    return None
-
-def sendTask(device, update, tests):
-    queue = []
-    msg = Message(6003)
-    updateUrl = getUpdateUrl(device)
-    if updateUrl and update:
-        queue.append({'type': 7001, 'updateUrl': updateUrl[0], 'buildDate': updateUrl[1]})
-    #date = datetime.datetime.now().strftime('%Y-%m-%d')
-    timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-    paths = (device.deviceId, device.build, device.buildDate.replace('-', '').replace(':', '').replace(' ', ''), timestamp)
-    items= []
-    for test in tests:
-        d = dict(json.loads(test))
-        i = d['id']
-        t = d['type']
-        params = d['params']
-        if t == 7001:
-            items.append({
-                'id': i,
-                'name': 'monkey',
-                'cmd': 'monkey -s 25 --ignore-timeouts --ignore-crashes -v {0} --pct-nav 0 --pct-trackball 0'.format(params),
-                'uploadPath': '/pub/log/{0}/{1}/{2}/{3}-monkey.txt'.format(*paths)
-            })
-        elif t == 7002:
-            items.append({
-                'id': i,
-                'name': 'monkey-com.android.gallery3d',
-                'cmd': 'sh start_monkey_exit.sh com.android.gallery3d com.android.camera.CameraLauncher {0}'.format(params),
-                'uploadPath': '/pub/log/{0}/{1}/{2}/{3}-monkey-com.android.gallery3d.txt'.format(*paths)
-            })
-        elif t == 7003:
-            items.append({
-                'id': i,
-                'name': 'monkey-com.android.contacts.people',
-                'cmd': 'sh start_monkey_exit.sh com.android.contacts com.android.contacts.activities.PeopleActivity {0}'.format(params),
-                'uploadPath': '/pub/log/{0}/{1}/{2}/{3}-monkey-com.android.contacts.people.txt'.format(*paths)
-            })
-        elif t == 7004:
-            items.append({
-                'id': i,
-                'name': 'monkey-com.android.mms',
-                'cmd': 'sh start_monkey_exit.sh com.android.mms com.android.mms.ui.FunctionSwitchActivity {0}'.format(params),
-                'uploadPath': '/pub/log/{0}/{1}/{2}/{3}-monkey-com.android.mms.txt'.format(*paths)
-            })
-        elif t == 7005:
-            items.append({
-                'id': i,
-                'name': 'monkey-com.android.contacts.dialer',
-                'cmd': 'sh start_monkey_exit.sh com.android.contacts com.android.contacts.DialerActivity {0}'.format(params),
-                'uploadPath': '/pub/log/{0}/{1}/{2}/{3}-monkey-com.android.contacts.dialer.txt'.format(*paths)
-            })
-        elif t == 7006:
-            items.append({
-                'id': i,
-                'name': 'monkey-com.android.settings',
-                'cmd': 'sh start_monkey_exit.sh com.android.settings com.android.settings.Settings {0}'.format(params),
-                'uploadPath': '/pub/log/{0}/{1}/{2}/{3}-monkey-com.android.settings.txt'.format(*paths)
-            })
-        elif t == 7007:
-            items.append({
-                'id': i,
-                'name': 'monkey-com.android.strengthenmusic',
-                'cmd': 'sh start_monkey_exit.sh com.android.strengthenmusic com.android.strengthenmusic.ui.MusicWelcome {0}'.format(params),
-                'uploadPath': '/pub/log/{0}/{1}/{2}/{3}-monkey-com.android.strengthenmusic.txt'.format(*paths)
-            })
-        elif t == 7008:
-            items.append({
-                'id': i,
-                'name': 'stress',
-                'cmd': 'am start -n com.ztemt.test.auto/.AutoTestActivity --es mode auto {0}'.format(params),
-                'uploadPath': '/pub/log/{0}/{1}/{2}/{3}-stress.txt'.format(*paths)
-            })
-    queue.append({'type': 7002, 'tests': str(items)})
-    msg.put('queue', queue)
-    print(str(msg))
-    device.protocol.sendMessage(str(msg).encode('utf-8'))
 
 class TestPlatformProtocol(WebSocketServerProtocol):
 
@@ -223,17 +107,7 @@ class TestPlatformProtocol(WebSocketServerProtocol):
             text = payload.decode('utf-8')
             d = dict(json.loads(text))
             if d['type'] == 6003:
-                tests = [
-                    '{"id": 1, "type": 7001, "params": "200000"}',
-                    '{"id": 2, "type": 7002, "params": "10000 10"}',
-                    '{"id": 3, "type": 7003, "params": "10000 10"}',
-                    '{"id": 4, "type": 7004, "params": "10000 10"}',
-                    '{"id": 5, "type": 7005, "params": "10000 10"}',
-                    '{"id": 6, "type": 7006, "params": "10000 10"}',
-                    '{"id": 7, "type": 7007, "params": "10000 10"}',
-                    '{"id": 8, "type": 7008, "params": "--ei times 10"}'
-                ]
-                sendTask(devices[self.deviceId], True, tests)
+                message.sendTask(devices[self.deviceId])
 
     def onClose(self, wasClean, code, reason):
         print('WebSocket connection closed: {0}'.format(reason))
